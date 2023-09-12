5a765e1d66bfcf99b59ad401d92c69fa6cd66fe4
Naira <nairaaleqsan.yan94@gmail.com>
2023-09-12 08:09:41 +0400

    fixed repository

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
+
 Route::post('/posts/{postId}/comments', [CommentController::class, 'store'])->middleware('auth')->name('comments.store');
 Route::get('/posts/{postId}', [CommentController::class, 'show'])->name('posts.show');
 
